96ebb8b47a2a0dd53d2fd1ad0e481eaaefc4bd58
avirozen <117524971+avirozen@users.noreply.github.com>
2024-01-21 16:40:41 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 basic.clearScreen()
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 0) {
-        basic.showIcon(IconNames.SmallDiamond)
+        basic.showIcon(IconNames.Chessboard)
         pins.digitalWritePin(DigitalPin.P1, 1)
     } else {
-        basic.showIcon(IconNames.Chessboard)
+        basic.showIcon(IconNames.SmallDiamond)
         pins.digitalWritePin(DigitalPin.P1, 0)
     }
 })
